b7e1473085255f8f0c961c280523e92132faaec1
Szymon <szymon@MacBook-Pro-Mac-2.local>
2021-11-01 11:34:17 +0000

    Jest, supertest and express installed, app.js and listen.js created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,14 @@
+const request = require("supertest");
 const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
 const { seed } = require('../db/seeds/seed.js');
+const app = require('../app.js')
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe('All tests: ', () => {
+    describe('', () => {
+        it('')
+    })
+})
