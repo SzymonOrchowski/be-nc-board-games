74a75b992f7ba213a379b71ffbf26a7e6004d847
Szymon <szymon@MacBook-Pro-Mac-2.local>
2021-11-01 12:41:52 +0000

    table user created with columns and tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,8 +13,18 @@ describe('All tests: ', () => {
             return db.query('SELECT * FROM categories').then(data => {
                 expect(data.fields[0].name).toBe('slug');
                 expect(data.fields[1].name).toBe('description')
+            }).catch(err => {
+                expect(err).toBe(undefined)
             })
-            
+        })
+        test('Check if seed.sql file creates table users, and it has columns: username, avatar_url, name', () => {
+            return db.query('SELECT * FROM users').then(data => {
+                expect(data.fields[0].name).toBe('username');
+                expect(data.fields[1].name).toBe('avatar_url')
+                expect(data.fields[2].name).toBe('name')
+            }).catch(err => {
+                expect(err).toBe(undefined)
+            }) 
         })
     })
 })
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -4,12 +4,22 @@ const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
   // 1. create tables
   return db
-    .query('DROP TABLE IF EXISTS categories')
+    .query('DROP TABLE IF EXISTS categories;')
+    .then(()=>{
+      return db.query('DROP TABLE IF EXISTS users;')
+    })
     .then(()=>{
       return db.query(`CREATE TABLE categories (
         slug VARCHAR NOT NULL UNIQUE PRIMARY KEY,
         description VARCHAR NOT NULL);`
       )
+    })
+    .then(()=>{
+      return db.query(`CREATE TABLE users (
+        username VARCHAR NOT NULL UNIQUE PRIMARY KEY,
+        avatar_url VARCHAR NOT NULL,
+        name VARCHAR NOT NULL
+      );`)
     })
   // 2. insert data
 };
