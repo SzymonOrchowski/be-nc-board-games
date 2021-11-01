007ba880be21f56fb9cc3929b32f0820db8d8b84
Szymon <szymon@MacBook-Pro-Mac-2.local>
2021-11-01 13:18:15 +0000

    All tables created and tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,33 +12,45 @@ describe('All tests: ', () => {
         test('Check if seed.sql file creates table categories, and has columns: slug, description', () => {
             return db.query('SELECT * FROM categories').then(data => {
                 expect(data.fields[0].name).toBe('slug');
-                expect(data.fields[1].name).toBe('description')
+                expect(data.fields[1].name).toBe('description');
             }).catch(err => {
-                expect(err).toBe(undefined)
+                expect(err).toBe(undefined);
             })
         })
         test('Check if seed.sql file creates table users, and it has columns: username, avatar_url, name', () => {
             return db.query('SELECT * FROM users').then(data => {
                 expect(data.fields[0].name).toBe('username');
-                expect(data.fields[1].name).toBe('avatar_url')
-                expect(data.fields[2].name).toBe('name')
+                expect(data.fields[1].name).toBe('avatar_url');
+                expect(data.fields[2].name).toBe('name');
             }).catch(err => {
-                expect(err).toBe(undefined)
+                expect(err).toBe(undefined);
             }) 
         })
         test('Check if seed.sql file creates table reviews, and it has columns: review_id, title, review_body, designer, review_img_url, votes, category, owner, created_at', () => {
             return db.query('SELECT * FROM reviews').then(data => {
                 expect(data.fields[0].name).toBe('review_id');
-                expect(data.fields[1].name).toBe('title')
-                expect(data.fields[2].name).toBe('review_body')
-                expect(data.fields[3].name).toBe('designer')
-                expect(data.fields[4].name).toBe('review_img_url')
-                expect(data.fields[5].name).toBe('votes')
-                expect(data.fields[6].name).toBe('category')
-                expect(data.fields[7].name).toBe('owner')
-                expect(data.fields[8].name).toBe('created_at')
+                expect(data.fields[1].name).toBe('title');
+                expect(data.fields[2].name).toBe('review_body');
+                expect(data.fields[3].name).toBe('designer');
+                expect(data.fields[4].name).toBe('review_img_url');
+                expect(data.fields[5].name).toBe('votes');
+                expect(data.fields[6].name).toBe('category');
+                expect(data.fields[7].name).toBe('owner');
+                expect(data.fields[8].name).toBe('created_at');
             }).catch(err => {
-                expect(err).toBe(undefined)
+                expect(err).toBe(undefined);
+            }) 
+        })
+        test('Check if seed.sql file creates table comments, and it has columns: comment_id, author, review_id, votes, created_at, body', () => {
+            return db.query('SELECT * FROM comments').then(data => {
+                expect(data.fields[0].name).toBe('comment_id');
+                expect(data.fields[1].name).toBe('author');
+                expect(data.fields[2].name).toBe('review_id');
+                expect(data.fields[3].name).toBe('votes');
+                expect(data.fields[4].name).toBe('created_at');
+                expect(data.fields[5].name).toBe('body');
+            }).catch(err => {
+                expect(err).toBe(undefined);
             }) 
         })
     })
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -4,7 +4,10 @@ const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
   // 1. create tables
   return db
-    .query('DROP TABLE IF EXISTS reviews;')
+    .query('DROP TABLE IF EXISTS comments;')
+    .then(()=>{
+      return db.query('DROP TABLE IF EXISTS reviews;')
+    })
     .then(()=>{
       return db.query('DROP TABLE IF EXISTS categories;')
     })
@@ -31,10 +34,20 @@ const seed = (data) => {
         review_body TEXT NOT NULL,
         designer VARCHAR NOT NULL,
         review_img_url VARCHAR DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
-        votes VARCHAR DEFAULT 0,
+        votes INT DEFAULT 0,
         category VARCHAR REFERENCES categories(slug),
         owner VARCHAR REFERENCES users(username),
         created_at TIMESTAMP
+      );`)
+    })
+    .then(()=>{
+      return db.query(`CREATE TABLE comments (
+        comment_id SERIAL PRIMARY KEY,
+        author VARCHAR REFERENCES users(username),
+        review_id INT REFERENCES reviews(review_id),
+        votes INT DEFAULT 0,
+        created_at TIMESTAMP,
+        body TEXT NOT NULL
       );`)
     })
   // 2. insert data
